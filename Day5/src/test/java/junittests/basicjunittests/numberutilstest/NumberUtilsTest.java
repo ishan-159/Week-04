2393a3b02d4d3a3b21efa18749d5b9b94d37c897
ishan-159 <ishantiwari159@gmail.com>
2025-02-15 23:45:02 +0530

    [Ishan] Add.J-Unit testing done for parameterized tests.

---
+++ Day5/src/test/java/junittests/basicjunittests/numberutilstest/NumberUtilsTest.java
@@ -0,0 +1,18 @@
+package junittests.basicjunittests.numberutilstest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumberUtilsTest {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    void testIsEvenTrue(int number) {
+        assertTrue(NumberUtils.isEven(number), number + " should be even.");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})
+    void testIsEvenFalse(int number) {
+        assertFalse(NumberUtils.isEven(number), number + " should be odd.");
+    }
+}
