95ce24a47ada29c8e400849272c5534f9f4b9081
ishan-159 <ishantiwari159@gmail.com>
2025-02-15 23:46:37 +0530

    [Ishan] Add.J-Unit testing done for performance testing using @Timeout.

---
+++ Day5/src/test/java/junittests/basicjunittests/performancetestertest/PerformanceTesterTest.java
@@ -0,0 +1,14 @@
+package junittests.basicjunittests.performancetestertest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
+
+class PerformanceTesterTest {
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    void testLongRunningTaskTimeout() {
+        PerformanceTester.longRunningTask();
+    }
+}
