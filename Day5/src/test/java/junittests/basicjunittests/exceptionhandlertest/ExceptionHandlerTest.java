786fa85e827822e9c48c47accd7fbbc94e452132
ishan-159 <ishantiwari159@gmail.com>
2025-02-15 23:42:13 +0530

    [Ishan] Add.J-Unit testing done for exception handling.

---
+++ Day5/src/test/java/junittests/basicjunittests/exceptionhandlertest/ExceptionHandlerTest.java
@@ -0,0 +1,22 @@
+package junittests.basicjunittests.exceptionhandlertest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ExceptionHandlerTest {
+
+    @Test
+    void testDivision() {
+        assertEquals(5, ExceptionHandler.divide(10, 2));
+        assertEquals(-3, ExceptionHandler.divide(-9, 3));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            ExceptionHandler.divide(10, 0);
+        });
+
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+}
